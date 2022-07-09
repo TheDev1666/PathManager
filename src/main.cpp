ac6761108730c1fd2a2e2093aa109aa117f64348
Kevin Hautmann <kevin.hautmann@outlook.fr>
2022-07-09 21:49:51 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	const std::string menuTitle{ "---------- CHOOSE AN OPTION ----------\n" };
+	std::cout << menuTitle << std::endl;
+
+	std::cout << "1 - Directly" 
+
+	return 0;
+}
