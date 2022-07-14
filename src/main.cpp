8d2f46e9292c23ec11d932771662c0dcec3e7aeb
TheDev1666 <ferresnowy@gmail.com>
2022-07-14 19:39:58 +0200

    Ajout de deux fonctions template de saisie

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 
-template <typename T>
-void entry(T& var)
+//Find relative path between two files or directory path
+void findRelativePathMenu()
 {
-	while (!std::cin >> var)
+	std::cout << "---------- FIND RELATIVE PATH BETWEEN TWO ABSOLUTE PATH ----------\n" << std::endl;
+}
+
+template <typename T, typename Pred>
+void entry(T& var, const Pred& predicat)
+{
+	while (!(std::cin >> var) || !predicat(var))
 	{
 		std::cerr << "Invalid entry, please retry !" << std::endl;
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+}
+
+template <typename T, typename Pred>
+void entry(T& var, const Pred& predicat, const std::string& errorMsg)
+{
+	while (!(std::cin >> var) || !predicat(var))
+	{
+		std::cerr << errorMsg;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
 }
 
 template <typename T>
-void entry(T& var, const std::string& invalidEntryMsg)
+void entry(T& var)
 {
-	while (!std::cin >> var)
+	while (!(std::cin >> var))
 	{
-		std::cerr << invalidEntryMsg << std::endl;
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cerr << "Invalid entry, please retry !" << std::endl;
 		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
 }
 
@@ -30,8 +47,19 @@ int main()
 	std::cout << "1 - Find out relative path of a directory" << std::endl;
 	std::cout << "2 - Find out absolute path of a directory" << std::endl;
 
+	std::cout << "Your choice : ";
+
 	unsigned choice = 0;
-	entry(choice);
+	entry(choice, [](int i) -> bool {return i >= 1 && i <= 2; }, "Your choice : ");
+
+	if (choice == 1)
+	{
+		findRelativePathMenu();
+	}
+	else if(choice == 2)
+	{
+
+	}
 
 	return 0;
 }
